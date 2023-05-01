e6ac267b9afe9cd257774ba8703b12c0d5c09867
tanakorncode <tanakorncode@gmail.com>
2023-05-02 06:58:16 +0700

    bugfix

--- api/index.py
+++ api/index.py
@@ -4,15 +4,18 @@ from typing import Union, Literal, List
 from PyPDF2 import PdfWriter, PdfReader
 from flask_restful import Resource, Api
 
-import os, sys
+import os
+import sys
 from flask import Flask, flash, request, redirect, url_for, render_template, send_from_directory, send_file
 from werkzeug.utils import secure_filename
+from os.path import join
 
-UPLOAD_FOLDER = './uploads'
+UPLOAD_FOLDER = 'uploads'
 ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
 
 def watermark(
     content_pdf: Path,
@@ -22,9 +25,9 @@ def watermark(
     page_indices: Union[Literal["ALL"], List[int]] = "ALL",
 ):
     if(password):
-      reader = PdfReader(content_pdf, False, password)
+        reader = PdfReader(content_pdf, False, password)
     else:
-      reader = PdfReader(content_pdf)
+        reader = PdfReader(content_pdf)
 
     if page_indices == "ALL":
         page_indices = list(range(0, len(reader.pages)))
@@ -45,13 +48,16 @@ def watermark(
     with open(pdf_result, "wb") as fp:
         writer.write(fp)
 
+
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
+
 @app.route('/')
 def home():
     return 'Hello, World!'
+
 
 @app.route('/watermark-pdf', methods=['GET', 'POST'])
 def upload_file():
@@ -68,12 +74,14 @@ def upload_file():
             return redirect(request.url)
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            file.save(join(app.config['UPLOAD_FOLDER'], filename))
 
-            watermark(os.path.join(app.config['UPLOAD_FOLDER'], filename), './files/'+request.form.get('template'), "pdf_result.pdf", request.form.get('password'))
-            return send_from_directory('..', "pdf_result.pdf")
+            watermark(join(app.config['UPLOAD_FOLDER'], filename), join('files', request.form.get(
+                'template')), join('files', "pdf_result.pdf"), request.form.get('password'))
+            return send_file(join('..', 'files', "pdf_result.pdf"), mimetype='application/pdf')
             # return redirect(url_for('download_file', name=filename))
     return render_template('upload.html')
+
 
 @app.route('/about')
 def about():
